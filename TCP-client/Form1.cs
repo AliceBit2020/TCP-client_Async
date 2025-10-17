60ceb6ea79d7a7af815f5dd113e0adeea77789cc
OleksandraT <syatle13@gmail.com>
2025-10-17 20:14:44 +0300

    client async

--- TCP-client/Form1.cs
+++ TCP-client/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace TCP_client
 {
@@ -59,16 +60,18 @@ namespace TCP_client
             ConnectAsync();            
         }
 
-        private async void ExchangeAsync()
+        private async Task ExchangeAsync()
         {
-            await Task.Run(() =>
+           await Task.Run( () =>
             {
                 try
                 {
                     ////
                     string theMessage = textBox1.Text; // получим текст сообщения, введенный в текстовое поле
                     byte[] msg = Encoding.Default.GetBytes(theMessage); // конвертируем строку, содержащую сообщение, в массив байтов
+                    Thread.Sleep(5000);
                     int bytesSent = sock.Send(msg); // отправляем серверу сообщение через сокет
+                   
                     if (theMessage.IndexOf("<end>") > -1) // если клиент отправил эту команду, то принимаем сообщение от сервера
                     {
                         byte[] bytes = new byte[1024];
@@ -82,12 +85,107 @@ namespace TCP_client
                 {
                     MessageBox.Show("Клиент: " + ex.Message);
                 }
-            }); 
+            });
+
+         
+
+         
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private async Task<int> ExchangeAsyncInt()
         {
-            ExchangeAsync();
+            await Task.Run(() =>
+            {
+                try
+                {
+                    ////
+                    string theMessage = textBox1.Text; // получим текст сообщения, введенный в текстовое поле
+                    byte[] msg = Encoding.Default.GetBytes(theMessage); // конвертируем строку, содержащую сообщение, в массив байтов
+                    Thread.Sleep(5000);
+                    int bytesSent = sock.Send(msg); // отправляем серверу сообщение через сокет
+
+                    if (theMessage.IndexOf("<end>") > -1) // если клиент отправил эту команду, то принимаем сообщение от сервера
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = sock.Receive(bytes); // принимаем данные, переданные сервером. Если данных нет, поток блокируется
+                        MessageBox.Show("Сервер (" + sock.RemoteEndPoint.ToString() + ") ответил: " + Encoding.Default.GetString(bytes, 0, bytesRec) /*конвертируем массив байтов в строку*/);
+                        sock.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                        sock.Close(); // закрываем сокет
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Клиент: " + ex.Message);
+                }
+            });
+            return 1;
+
+
+
+
+        }
+        private async Task<int> ExchangeAsyncTaskInt()
+        {
+            int th_id = Thread.CurrentThread.ManagedThreadId;
+            MessageBox.Show(th_id.ToString());
+            var result= await Task.Run<int>(() =>
+            {
+                try
+                {
+                    ////
+                    string theMessage = textBox1.Text; // получим текст сообщения, введенный в текстовое поле
+                    byte[] msg = Encoding.Default.GetBytes(theMessage); // конвертируем строку, содержащую сообщение, в массив байтов
+                    Thread.Sleep(5000);
+                    int bytesSent = sock.Send(msg); // отправляем серверу сообщение через сокет
+
+                    if (theMessage.IndexOf("<end>") > -1) // если клиент отправил эту команду, то принимаем сообщение от сервера
+                    {
+                        byte[] bytes = new byte[1024];
+                        int bytesRec = sock.Receive(bytes); // принимаем данные, переданные сервером. Если данных нет, поток блокируется
+                        MessageBox.Show("Сервер (" + sock.RemoteEndPoint.ToString() + ") ответил: " + Encoding.Default.GetString(bytes, 0, bytesRec) /*конвертируем массив байтов в строку*/);
+                        sock.Shutdown(SocketShutdown.Both); // Блокируем передачу и получение данных для объекта Socket.
+                        sock.Close(); // закрываем сокет
+                    }
+                    return bytesSent;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Клиент: " + ex.Message);
+                    return 0;
+                }
+            });
+            th_id = Thread.CurrentThread.ManagedThreadId;
+            MessageBox.Show(th_id.ToString());
+            return result;
+
+
+
+
+        }
+
+        //private  void button3_Click(object sender, EventArgs e)
+        //{
+        //    ExchangeAsync();
+            
+        //}
+        //private async void button4_Click(object sender, EventArgs e)
+        //{
+        //  int res=await ExchangeAsyncInt();
+        //    MessageBox.Show(res.ToString());
+        //}
+        private async void button3_Click(object sender, EventArgs e)
+        {
+
+            int th_id = Thread.CurrentThread.ManagedThreadId;
+            MessageBox.Show("Click before await"+th_id.ToString());
+
+            int res = await ExchangeAsyncTaskInt();
+            MessageBox.Show(res.ToString());
+
+          th_id = Thread.CurrentThread.ManagedThreadId;
+            MessageBox.Show("Click after await"+th_id.ToString());
+
+
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
